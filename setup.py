1f3f27b8593d99cecbfa2d80e06997ee32bdd46c
vallirajasekar <sekarvalliraja@gmail.com>
2024-04-19 17:58:03 +0530

    version updated_8

--- setup.py
+++ setup.py
@@ -1,42 +1,42 @@
-from setuptools import setup, find_packages
-from typing import List
+    from setuptools import setup, find_packages
+    from typing import List
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()   
-    
-    
-HYPEN_E_DOT='-e .'
-
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+    with open('README.md', 'r', encoding='utf-8') as f:
+        long_description = f.read()   
         
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements  
-   
+        
+    HYPEN_E_DOT='-e .'
 
-__version__ = "0.0.8"
-REPO_NAME = "mongodb_connector_package"
-PKG_NAME= "databaseautomationmongodb_connector"
-AUTHOR_USER_NAME = "vallirajasekar"
-AUTHOR_EMAIL = "sekarvalliraja@gmail.com"
+    def get_requiremet(file_path:str)->List[str]:
+        requirements = []
+        with open(file_path) as f:
+            requirements=f.readlines()
+            requirements=[req.replace("\n","")for req in requirements]
+            
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+        return requirements  
+    
 
-setup(
-    name=PKG_NAME,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",  # corrected parameter name
+    __version__ = "0.0.8"
+    REPO_NAME = "mongodb_connector_package"
+    PKG_NAME= "databaseautomation"
+    AUTHOR_USER_NAME = "vallirajasekar"
+    AUTHOR_EMAIL = "sekarvalliraja@gmail.com"
 
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    )
+    setup(
+        name=PKG_NAME,
+        version=__version__,
+        author=AUTHOR_USER_NAME,
+        author_email=AUTHOR_EMAIL,
+        description="A python package for connecting with database.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",  # corrected parameter name
+
+        url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+        project_urls={
+            "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        },
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
+        )
