5e7dd7ca4f08723ad82f23557a9e589c2d6b505b
Valli Raja Sekar <122970360+vallirajasekar@users.noreply.github.com>
2024-04-20 20:17:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-    from setuptools import setup, find_packages
-    from typing import List
+from setuptools import setup, find_packages
+from typing import List
 
-    with open('README.md', 'r', encoding='utf-8') as f:
-        long_description = f.read()   
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()   
         
         
-    HYPEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 
-    def get_requiremet(file_path:str)->List[str]:
-        requirements = []
-        with open(file_path) as f:
-            requirements=f.readlines()
-            requirements=[req.replace("\n","")for req in requirements]
+def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
             
-            if HYPEN_E_DOT in requirements:
-                requirements.remove(HYPEN_E_DOT)
-        return requirements  
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements  
     
 
-    __version__ = "0.0.8"
-    REPO_NAME = "mongodb_connector_package"
-    PKG_NAME= "databaseautomation"
-    AUTHOR_USER_NAME = "vallirajasekar"
-    AUTHOR_EMAIL = "sekarvalliraja@gmail.com"
+__version__ = "0.0.8"
+REPO_NAME = "mongodb_connector_package"
+PKG_NAME= "databaseautomation"
+AUTHOR_USER_NAME = "vallirajasekar"
+AUTHOR_EMAIL = "sekarvalliraja@gmail.com"
 
-    setup(
+setup(
         name=PKG_NAME,
         version=__version__,
         author=AUTHOR_USER_NAME,
